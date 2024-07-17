30447a23fe1f3db3ef1dfe85b14c3972f3a73c50
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2024-07-17 11:10:50 +0530

    Create Lecture30.cpp

---
+++ Lecture30.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class bank_client{
+private:
+	int credit_card;
+	char pin_number;
+	string account_name;
+public:
+	void set_credit_number(int number){
+		credit_card=number;
+	}
+	void get_credit_number(){
+		cout<<credit_card;
+	}
+	bank_client(){
+		cout<<credit_card;
+	}
+};
+int main(){
+	bank_client e;
+e.set_credit_number(56789);
+e.get_credit_number();
+}
